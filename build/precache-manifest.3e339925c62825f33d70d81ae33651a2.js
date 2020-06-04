ed278230632614f961eb57f548896270954ec365
hellow-maira <63617730+hellow-maira@users.noreply.github.com>
2020-06-04 02:13:37 -0300

    tentativa 03 de rodar a pagina

---
+++ build/precache-manifest.3e339925c62825f33d70d81ae33651a2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938a4c32c5f05c8cc1d896b81bc6a3f8",
+    "url": "/ladingpage-final/index.html"
+  },
+  {
+    "revision": "f65d2c53d9889645d513",
+    "url": "/ladingpage-final/static/css/main.11eb9c65.chunk.css"
+  },
+  {
+    "revision": "a2f3db5266942f4e939f",
+    "url": "/ladingpage-final/static/js/2.dfa803f2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ladingpage-final/static/js/2.dfa803f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f65d2c53d9889645d513",
+    "url": "/ladingpage-final/static/js/main.d9657918.chunk.js"
+  },
+  {
+    "revision": "22006251b19a3633e763",
+    "url": "/ladingpage-final/static/js/runtime-main.e4927dde.js"
+  },
+  {
+    "revision": "10d22939946a9bc341c07862b88e0cba",
+    "url": "/ladingpage-final/static/media/Img.10d22939.jpg"
+  },
+  {
+    "revision": "8e82115d3abf0ce71ae7feb0b2787111",
+    "url": "/ladingpage-final/static/media/Meeting.8e82115d.svg"
+  },
+  {
+    "revision": "e0715f02b86c6fe7b0a163888fe56f49",
+    "url": "/ladingpage-final/static/media/cardescolha.e0715f02.svg"
+  },
+  {
+    "revision": "143ebb232cd4beb31df9f7a8c2b217a3",
+    "url": "/ladingpage-final/static/media/cardfoguete2.143ebb23.svg"
+  },
+  {
+    "revision": "4f457608e6746ecd90a740307e6c0b37",
+    "url": "/ladingpage-final/static/media/cardwebsite.4f457608.svg"
+  },
+  {
+    "revision": "d0907d84428796ae5fc4add41f89d287",
+    "url": "/ladingpage-final/static/media/flogo.d0907d84.svg"
+  },
+  {
+    "revision": "9b384892dff585c5565b8344afdc601a",
+    "url": "/ladingpage-final/static/media/landing.9b384892.svg"
+  },
+  {
+    "revision": "e9a202d85f0bc46820cc9fd00273e5d9",
+    "url": "/ladingpage-final/static/media/perguntas.e9a202d8.svg"
+  },
+  {
+    "revision": "6be16daecdcba52c17b0a287bd75419b",
+    "url": "/ladingpage-final/static/media/personalize.6be16dae.svg"
+  },
+  {
+    "revision": "67d2b0a2506f7c4401410eb2d30662c3",
+    "url": "/ladingpage-final/static/media/wordpress.67d2b0a2.svg"
+  }
+]);
